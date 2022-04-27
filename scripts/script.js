ef02b4e6be018843c40d461a1b56a730fb2f118e
Veronika Sergienko <nichka030791@yandex.ru>
2022-04-28 01:21:32 +0600

    feat: сверстать набросок сайта 1.2

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,14 @@ let EditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let ClouseButton = document.querySelector('.popup__clouse-button');
 
-EditButton.addEventListener('click', function() {
-    console.log('pabotaet');
+EditButton.addEventListener("click", function(event) {
+    togglePopup();
 });
+
+ClouseButton.addEventListener("click", function(event) {
+    togglePopup();
+});
+
+function togglePopup() {
+    popup.classList.toggle("popup_active");
+};
