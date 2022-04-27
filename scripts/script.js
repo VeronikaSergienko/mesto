b855999780f4c9f7b8a099c3980f540bc19d6e12
Veronika Sergienko <nichka030791@yandex.ru>
2022-04-27 21:17:31 +0600

    feat: сверстать набросок сайта 1.1

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+let EditButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let ClouseButton = document.querySelector('.popup__clouse-button');
+
+EditButton.addEventListener('click', function() {
+    console.log('pabotaet');
+});
