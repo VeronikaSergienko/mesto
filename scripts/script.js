9b522a5833cf7a51bf8b8f35366166d8146d73a3
Veronika Sergienko <nichka030791@yandex.ru>
2022-05-04 12:43:42 +0600

    исправления после ревью 1.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,35 @@
-let EditButton = document.querySelector('.profile__edit-button');
+let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form');
-let ClouseButton = document.querySelector('.popup__clouse-button');
-let SaveButton = document.querySelector('.popup__save-button');
-let ProfileName = document.querySelector('.profile__name');
-let ProfileTypeOfActivity = document.querySelector('.profile__type-of-activity');
-let nameInput = document.querySelector('#PopupName');
-let jobInput = document.querySelector('#PopupTypeOfActivity');
+let clouseButton = document.querySelector('.popup__clouse-button');
+let profileName = document.querySelector('.profile__name');
+let profileTypeOfActivity = document.querySelector('.profile__type-of-activity');
+let nameInput = document.querySelector('#popupName');
+let jobInput = document.querySelector('#popupTypeOfActivity');
 
-EditButton.addEventListener("click", function(event) {
-    openPopup();
-});
-
-ClouseButton.addEventListener("click", function(event) {
-    clousePopup();
-});
-
-SaveButton.addEventListener("click", function(event) {
-    ProfileName.textContent = nameInput.value;
-    ProfileTypeOfActivity.textContent = jobInput.value;
-    clousePopup();
-});
-
+// функция открытия формы редактирования профиля
 function openPopup() {
     popup.classList.add('popup_opened');
-    nameInput.value = ProfileName.textContent;
-    jobInput.value = ProfileTypeOfActivity.textContent;
-}
-
-function clousePopup() {
-    popup.classList.remove('popup_opened');
-}
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    ProfileName.textContent = nameInput.value;
-    ProfileTypeOfActivity.textContent = jobInput.value;
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileTypeOfActivity.textContent;
 };
 
-popup.addEventListener('submit', formSubmitHandler);
+// функция закрытия формы нажатием на "крестик"
+function clousePopup() {
+    popup.classList.remove('popup_opened');
+};
+
+// Обработчик «отправки» формы
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileTypeOfActivity.textContent = jobInput.value;
+    clousePopup();
+};
+
+editButton.addEventListener("click", openPopup);
+
+clouseButton.addEventListener("click", clousePopup);
+
+// Прикрепляем обработчик к форме
+formElement.addEventListener('submit', formSubmitHandler);
