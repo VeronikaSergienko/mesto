bb71257c447f346e7a381230d095947814cdb62d
Veronika Sergienko <nichka030791@yandex.ru>
2022-05-12 00:03:40 +0600

    сделать лайк

--- scripts/card.js
+++ scripts/card.js
@@ -1,3 +1,39 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
+let clouseButton = document.querySelector('.popup__clouse-button');
+let profileName = document.querySelector('.profile__name');
+let profileTypeOfActivity = document.querySelector('.profile__type-of-activity');
+let nameInput = document.querySelector('#popupName');
+let jobInput = document.querySelector('#popupTypeOfActivity');
+
+// функция открытия формы редактирования профиля
+function openPopup() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileTypeOfActivity.textContent;
+};
+
+// функция закрытия формы нажатием на "крестик"
+function clousePopup() {
+    popup.classList.remove('popup_opened');
+};
+
+// Обработчик «отправки» формы
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileTypeOfActivity.textContent = jobInput.value;
+    clousePopup();
+};
+
+editButton.addEventListener("click", openPopup);
+
+clouseButton.addEventListener("click", clousePopup);
+
+// Прикрепляем обработчик к форме
+formElement.addEventListener('submit', formSubmitHandler);
+
 const cardTemplate = document.querySelector('#card').content;
 const elements = document.querySelector('.elements');
 
@@ -28,11 +64,30 @@ const initialCards = [
     }
   ]; 
 
+  // создание карточки
+
   initialCards.forEach((item) => {
     const card = cardTemplate.querySelector('.element').cloneNode(true);
     card.querySelector('.element__text').textContent = item.name;
     const img = card.querySelector('.element__image');
     img.src = item.link;
     img.alt = `Фото ${item.name}.`;
+    // переключатель для лайка
+    card.querySelector('.element__like-button').addEventListener("click", function(evt) {
+      evt.target.classList.toggle('element__like-button_active');
+  });
+  // добавление карточки в начало
     elements.prepend(card);
   });
+
+  
+
+  // открытие попапа создания карточки
+  const addButton = document.querySelector('.profile__add-button');
+  const popupNewPlace = document.querySelector('.popup_new-place');
+
+  function openPopupNewPlace() {
+    popup.classList.add('popup_opened');
+  };
+
+  addButton.addEventListener('click', openPopupNewPlace);
--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,36 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__form');
-let clouseButton = document.querySelector('.popup__clouse-button');
-let profileName = document.querySelector('.profile__name');
-let profileTypeOfActivity = document.querySelector('.profile__type-of-activity');
-let nameInput = document.querySelector('#popupName');
-let jobInput = document.querySelector('#popupTypeOfActivity');
+// let editButton = document.querySelector('.profile__edit-button');
+// let popup = document.querySelector('.popup');
+// let formElement = document.querySelector('.popup__form');
+// let clouseButton = document.querySelector('.popup__clouse-button');
+// let profileName = document.querySelector('.profile__name');
+// let profileTypeOfActivity = document.querySelector('.profile__type-of-activity');
+// let nameInput = document.querySelector('#popupName');
+// let jobInput = document.querySelector('#popupTypeOfActivity');
 
-// функция открытия формы редактирования профиля
-function openPopup() {
-    popup.classList.add('popup_opened');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileTypeOfActivity.textContent;
-};
+// // функция открытия формы редактирования профиля
+// function openPopup() {
+//     popup.classList.add('popup_opened');
+//     nameInput.value = profileName.textContent;
+//     jobInput.value = profileTypeOfActivity.textContent;
+// };
 
-// функция закрытия формы нажатием на "крестик"
-function clousePopup() {
-    popup.classList.remove('popup_opened');
-};
+// // функция закрытия формы нажатием на "крестик"
+// function clousePopup() {
+//     popup.classList.remove('popup_opened');
+// };
 
-// Обработчик «отправки» формы
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileTypeOfActivity.textContent = jobInput.value;
-    clousePopup();
-};
+// // Обработчик «отправки» формы
+// function formSubmitHandler (evt) {
+//     evt.preventDefault();
+//     profileName.textContent = nameInput.value;
+//     profileTypeOfActivity.textContent = jobInput.value;
+//     clousePopup();
+// };
 
-editButton.addEventListener("click", openPopup);
+// editButton.addEventListener("click", openPopup);
 
-clouseButton.addEventListener("click", clousePopup);
+// clouseButton.addEventListener("click", clousePopup);
 
-// Прикрепляем обработчик к форме
-formElement.addEventListener('submit', formSubmitHandler);
+// // Прикрепляем обработчик к форме
+// formElement.addEventListener('submit', formSubmitHandler);
+
