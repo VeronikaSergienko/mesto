f04a2109b30fa157602ba6b4514d5543c1800455
Veronika Sergienko <nichka030791@yandex.ru>
2022-05-28 23:22:37 +0600

    наброски 2.0

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,47 +1,15 @@
-/* Требования к валидации форм. Разбейте код валидации на функции. 
-Вы уже делали это в теме «Валидация форм». Сделайте функцию enableValidation ответственной за включение валидации всех форм. 
-Пусть она принимает как объект настроек все нужные функциям классы и селекторы элементов:*/
-
-//  включение валидации вызовом enableValidation
-// все настройки передаются при вызове
-
-// enableValidation({
-//   formSelector: '.popup__form',
-//   inputSelector: '.popup__input',
-//   submitButtonSelector: '.popup__button',
-//   inactiveButtonClass: 'popup__button_disabled',
-//   inputErrorClass: 'popup__input_type_error',
-//   errorClass: 'popup__error_visible'
-// }); 
-
 const formElement = document.querySelector('.popup__form');
 const formInput = formElement.querySelector('.popup__input');
-// Выбираем элемент ошибки на основе уникального класса 
 const formError = formElement.querySelector(`.${formInput.id}-error`);
 
-formElement.addEventListener('submit', function (evt) {
-    // Отменим стандартное поведение
-    evt.preventDefault();
-});
-  
-  // Слушатель события input
-  formInput.addEventListener('input', function (evt) {
-    // Выведем в консоль значение свойства validity.valid поля ввода, 
-    // на котором слушаем событие input
-    console.log(evt.target.validity.valid);
-});
-
 const showInputError = (element, errorMessage) => {
-    element.classList.add('popup__input-error');
-    // Показываем сообщение об ошибке
-      // Заменим содержимое span с ошибкой на переданный параметр
-  formError.textContent = errorMessage;
+    element.classList.add('popup__input_type_error');
     formError.classList.add('popup__input-error_active');
+    formError.textContent = errorMessage;
 };
-  
+
 const hideInputError = (element) => {
-    element.classList.remove('popup__input-error');
-    // Скрываем сообщение об ошибке
+    element.classList.remove('form__input_type_error');
     formError.classList.remove('popup__input-error_active');
     formError.textContent = '';
 };
@@ -49,13 +17,15 @@ const hideInputError = (element) => {
 // Функция, которая проверяет валидность поля
 const isValid = () => {
     if (!formInput.validity.valid) {
-      // Если поле не проходит валидацию, покажем ошибку
       showInputError(formInput, formInput.validationMessage);
     } else {
-      // Если проходит, скроем
       hideInputError(formInput);
     }
-  };
+};
+
+formElement.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+});
 
 // Вызовем функцию isValid на каждый ввод символа
 formInput.addEventListener('input', isValid);
