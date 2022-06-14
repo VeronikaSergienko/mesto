54f3e10f082a904f8ec5e8cc6992aa2c874a5849
Veronika Sergienko <nichka030791@yandex.ru>
2022-06-14 20:10:54 +0600

    создать класс карточки

--- scripts/index.js
+++ scripts/index.js
@@ -79,34 +79,34 @@ cardCreationButton.addEventListener("click", (evt) => {
     });
 
 // создание новой карточки
-const makeCard = (item) => {
-  const card = cardTemplate.querySelector('.element').cloneNode(true);
-  card.querySelector('.element__text').textContent = item.name;
-  const image = card.querySelector('.element__image');
-  image.src = item.link;
-  image.alt = item.name;
-  card.querySelector('.element__like-button').addEventListener("click", (evt) => {
-    evt.target.classList.toggle('element__like-button_active');
-  });
-  card.querySelector('.element__delete-button').addEventListener("click", (evt) => {
-    const place = evt.target.closest('.element');
-    place.remove();
-  });
-  image.addEventListener("click", (evt) => {
-    const clickImage = evt.target;
-    placeImage.src = clickImage.src;
-    placeImage.alt = clickImage.alt;
-    placeTitle.textContent = clickImage.alt;
-    openPopup(popupPlaceImage);
-  });
-  return card;
-};
+// const makeCard = (item) => {
+//   const card = cardTemplate.querySelector('.element').cloneNode(true);
+//   card.querySelector('.element__text').textContent = item.name;
+//   const image = card.querySelector('.element__image');
+//   image.src = item.link;
+//   image.alt = item.name;
+//   card.querySelector('.element__like-button').addEventListener("click", (evt) => {
+//     evt.target.classList.toggle('element__like-button_active');
+//   });
+//   card.querySelector('.element__delete-button').addEventListener("click", (evt) => {
+//     const place = evt.target.closest('.element');
+//     place.remove();
+//   });
+//   image.addEventListener("click", (evt) => {
+//     const clickImage = evt.target;
+//     placeImage.src = clickImage.src;
+//     placeImage.alt = clickImage.alt;
+//     placeTitle.textContent = clickImage.alt;
+//     openPopup(popupPlaceImage);
+//   });
+//   return card;
+// };
 
 // добавление карточки
-function addCard(item) {
-  const newCard = makeCard(item);
-  listOfCards.prepend(newCard);
-};
+// function addCard(item) {
+//   const newCard = makeCard(item);
+//   listOfCards.prepend(newCard);
+// };
 
 // обработчик отправки формы нового места
   function submitTheFormNewPlace (evt) {
@@ -126,7 +126,7 @@ function addCard(item) {
 formPlace.addEventListener('submit', submitTheFormNewPlace);
 
 // создание карточек перебором из массива
-initialCards.forEach(addCard);
+// initialCards.forEach(addCard);
 
 // функция закрытия попапа кликом на overlay.
 // перебирает массив попапов, вешает слушатели. Если при клике у попапа есть класс 'открытый папап', то закрывает попап.
@@ -146,3 +146,62 @@ function addPlaceWhenClickingOnEnter(evt) {
 };
 
 formPlace.addEventListener('keydown', addPlaceWhenClickingOnEnter);
+
+class Card {
+
+  _element = '#element';
+
+  constructor(item) {
+    this._name = item.name,
+    this._link = item.link
+  }
+
+  _getElement() {
+    const card = cardTemplate.querySelector(this._element).cloneNode(true);
+    return card;
+  }
+
+  _toggleLike() {
+    this._likeButton.addEventListener("click", (evt) => {
+      evt.target.classList.toggle('element__like-button_active');
+    });    
+  }
+
+  _deleteButton() {
+    this._buttonDelete.addEventListener("click", (evt) => {
+      const place = evt.target.closest('.element');
+      place.remove();
+    });
+  }
+
+  _openCardImage() {
+    this._image.addEventListener("click", (evt) => {
+      const clickImage = evt.target;
+      placeImage.src = clickImage.src;
+      placeImage.alt = clickImage.alt;
+      placeTitle.textContent = clickImage.alt;
+      openPopup(popupPlaceImage);
+    });
+  }
+
+  generate() {
+    this._element = this._getElement();
+    this._element.querySelector(".element__text").textContent = this._name;
+    this._image = this._element.querySelector('.element__image');
+    this._image.src = this._link;
+    this._image.alt = this._name;
+    this._openCardImage();
+    this._buttonDelete = this._element.querySelector('.element__delete-button');
+    this._deleteButton();
+    this._likeButton = this._element.querySelector('.element__like-button');
+    this._toggleLike();
+    return this._element;
+  }
+  
+}
+
+initialCards.forEach((item) => {
+  const Card1 = new Card(item);
+  const card = Card1.generate();
+  listOfCards.prepend(card);
+});
